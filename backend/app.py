d2ead9ce35e23341122c46c0c8622e4abb0022b3
Tung Nguyen <hoangtungtcc@gmail.com>
2023-06-10 09:20:52 +0700

    Add signup and login api

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,21 @@
 import os
-from flask import Flask, abort, jsonify
-from models import setup_db, User, Project
+from flask import Flask, abort, jsonify, request
+from models import setup_db, User, Project, db
 from flask_cors import CORS
+from hashlib import sha256
+import jwt
+import datetime
+
+JWT_SECRET = os.environ.get('JWT_SECRET', 'abc123abc1234')
+
+def _get_jwt(user_id):
+    exp_time = datetime.datetime.utcnow() + datetime.timedelta(hours=1)
+    payload = {
+        'exp': exp_time,
+        'nbf': datetime.datetime.utcnow(),
+        'id': user_id
+    }
+    return jwt.encode(payload, JWT_SECRET, algorithm='HS256')
 
 def create_app(test_config=None):
     app = Flask(__name__)
@@ -64,6 +78,87 @@ def create_app(test_config=None):
                 "projects": projects
             }
         )
+
+    @app.route('/signup', methods=['POST'])
+    def signup():
+        username = request.get_json().get('username')
+        email = request.get_json().get('email')
+        password = request.get_json().get('password')
+        if not username:
+            return jsonify(
+                {
+                    "message": "no username provided"
+                }, 400)
+        elif User.query.filter(User.username == username).one_or_none() is not None:
+            return jsonify(
+                {
+                    "message": "User existed"
+                }, 409)
+
+        if not email:
+            return jsonify(
+                {
+                    "message": "no email provided"
+                }, 400)
+        elif User.query.filter(User.email == email).one_or_none() is not None:
+            return jsonify(
+                {
+                    "message": "Email existed"
+                }, 409)
+
+        if not password:
+            return jsonify(
+                {
+                    "message": "no password provided"
+                }, 400)
+        password = sha256(password.encode('utf-8')).hexdigest()
+
+        newUser = User(username, password, email)
+        try:
+            db.session.add(newUser)
+            db.session.commit()
+        except:
+            abort(422)
+
+        return jsonify(
+            {
+                "success": True,
+                "message": "User was signed up successfully!",
+                "projects": newUser.format()
+            }
+        )
+
+    @app.route('/login', methods=['POST'])
+    def login():
+        username = request.get_json().get('username')
+        password = request.get_json().get('password')
+        if not username:
+            return jsonify(
+                {
+                    "message": "No user provided!"
+                }, 400)
+
+        qUser = User.query.filter(User.username == username).one_or_none()
+        if qUser is None:
+            return jsonify(
+                {
+                    "message": "User doesn't existed!"
+                }, 404)
+
+        if not password:
+            return jsonify(
+                {
+                    "message": "no password provided"
+                }, 400)
+
+        password = sha256(password.encode('utf-8')).hexdigest()
+        if password != qUser.password:
+            return jsonify(
+                {
+                    "message": "Wrong password!"
+                }, 401)
+        
+        return jsonify(token=_get_jwt(qUser.id))
 
     return app
 
